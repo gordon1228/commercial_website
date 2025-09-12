d3acb3160f9c842398d39dedbb0f5c3d009b2d6f
gordon1228 <gordonkwang1228@gmail.com>
2025-09-12 19:33:50 +0800

    Fix

--- prisma/migrations/20250909000000_add_mobile_images/migration.sql
+++
@@ -1,8 +0,0 @@
--- Migration: Add mobileImages column to vehicles table
--- Generated manually to fix schema drift
-
--- AlterTable
-ALTER TABLE "vehicles" ADD COLUMN IF NOT EXISTS "mobileImages" TEXT[] DEFAULT ARRAY[]::TEXT[];
-
--- Update existing vehicles to have empty mobileImages array if NULL
-UPDATE "vehicles" SET "mobileImages" = ARRAY[]::TEXT[] WHERE "mobileImages" IS NULL;
